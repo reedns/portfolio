941f9ec1d361b4e1ae041cc3a9652b8330a0feb1
Reed Nelson-Saunders <reedns@hotmail.com>
2014-09-21 10:08:37 -0700

    fix tests and delete redundant test

--- test/features/creating_an_article_test.rb
+++ test/features/creating_an_article_test.rb
@@ -3,10 +3,10 @@ require 'test_helper'
 feature 'creating and article' do
   scenario 'user creates a blog article' do
     visit new_article_path
-
+    save_and_open_page
     fill_in 'Title', with: articles(:article).title
-    fill_in 'Body', with: articles(:article).body
-    click_button 'Create'
+    fill_in 'article_body', with: articles(:article).body
+    click_button 'Save'
 
     page.must_have_content(articles(:article).body)
   end
--- test/features/editing_an_article_test.rb
+++ test/features/editing_an_article_test.rb
@@ -4,7 +4,8 @@ feature 'editing an article' do
   scenario 'user edits a blog article' do
 
     visit edit_article_path(articles(:article))
-    fill_in "Title", with: articles(:article).title
-    click_button "Edit"
+    fill_in "Title", with: "New Title"
+    click_button "Save"
+    page.must_have_content "New Title"
   end
 end
