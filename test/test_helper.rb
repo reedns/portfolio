26f18454401c719c410d0012e09cfa9dcef62fb0
Reed Nelson-Saunders <reedns@hotmail.com>
2014-09-25 12:23:16 -0700

    Clean up test_helper file and add codeship badge to readme

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,34 +1,25 @@
 ENV['RAILS_ENV'] = 'test'
-# puts Rails.env
+puts Rails.env
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'minitest/rails'
 require 'minitest/rails/capybara'
 require 'minitest/pride'
+require 'capybara/poltergeist'
 
 class ActiveSupport::TestCase
   ActiveRecord::Migration.check_pending!
-
-  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
   fixtures :all
-  # Add more helper methods to be used by all tests here...
-end
 
-class FeatureSpec < MiniTest::Spec
-  require 'capybara/poltergeist'
   include Capybara::DSL
   Capybara.javascript_driver = :poltergeist
-  register_spec_type(/page$/, self)
+  Capybara.ignore_hidden_elements = false
+
+  def sign_in
+    visit new_user_session_path
+    fill_in "Email", with: users(:jimbo).email
+    fill_in "Password", with: 'password'
+    click_on "Log in"
+  end
 end
 
-Capybara.ignore_hidden_elements = false
-
-def sign_in
-  visit new_user_session_path
-  fill_in "Email", with: users(:jimbo).email
-  fill_in "Password", with: 'password'
-  click_on "Log in"
-end
