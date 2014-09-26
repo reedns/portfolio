3fd3ffb88c4fc709dce36fa34686cb8a489d94d3
Reed Nelson-Saunders <reedns@hotmail.com>
2014-09-26 16:06:26 -0700

    fix zub test

--- test/features/zurb_is_part_of_view_test.rb
+++ test/features/zurb_is_part_of_view_test.rb
@@ -2,7 +2,9 @@ require 'test_helper'
 
 feature 'displaying page styling from zurb foundation' do
   scenario 'user visits welcome page' do
-    visit root_path
-    page.must_have_css 'script[src="/assets/vendor/modernizr.js"]'
+    visit articles_path
+    save_and_open_page
+    page.find "div[class='small-9 columns']"
   end
 end
+
