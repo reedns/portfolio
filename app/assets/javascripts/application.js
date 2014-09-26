8f54e8b1e98b6b7b5327e73b702264ea0cdfeb23
Reed Nelson-Saunders <reedns@hotmail.com>
2014-09-26 12:00:16 -0700

    continue work to integrate of theme

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,8 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require foundation
-//= /=require_tree ../../../vendor/assets/javascripts/
+//= ./../../vendor/assets/javascripts/
+//= require_tree .
 $(function() {
   $(document).foundation();
 });
