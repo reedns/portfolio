83caa0ce721f84df7dd1314626aee3bbd5fe8066
Reed Nelson-Saunders <reedns@hotmail.com>
2014-10-15 12:26:51 -0700

    fix rubocop offenses

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -5,7 +5,7 @@ class UserMailerTest < ActionMailer::TestCase
     email = UserMailer.contact_me_email(contacts(:contact)).deliver
     assert_not ActionMailer::Base.deliveries.empty?
 
-    assert_equal [users(:jimbo).email], email.from
+    assert_equal [contacts(:contact).email], email.from
     assert_equal ['reedness3000@gmail.com'], email.to
     assert_equal 'Contact request', email.subject
   end
